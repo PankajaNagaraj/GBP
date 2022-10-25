cc848b69062ffd0cfc1b8b4573adc5052b1331f4
PankajaNagaraj <pankajanagaraj@gmail.com>
2022-10-25 12:14:51 +0530

    created a test class under src/test

---
+++ src/test/java/com/qa/opencart/tests/testingGitpush.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.tests;
+
+import org.testng.annotations.Test;
+
+public class testingGitpush {
+	
+	@Test
+	public void test001() {
+		System.out.println("helooooooo");
+	}
+
+}
